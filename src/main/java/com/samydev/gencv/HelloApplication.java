47c4f0276dad95e0ca2650a0e0fcf5d60984cba2
Samy-Bodio2 <samybodio8@gmail.com>
2024-05-10 11:25:35 +0100

    push

--- src/main/java/com/samydev/gencv/HelloApplication.java
+++ src/main/java/com/samydev/gencv/HelloApplication.java
@@ -3,6 +3,7 @@ package com.samydev.gencv;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +11,12 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view2.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1550, 800);
+        Screen screen = Screen.getPrimary();
+        double screenWidth = screen.getBounds().getWidth();
+        double screenHeight = screen.getBounds().getHeight();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Main-Page.fxml"));
+        System.out.println( screenWidth);
+        Scene scene = new Scene(fxmlLoader.load(), screenWidth,screenHeight -66 );
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
